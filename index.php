cc9cb0e544078905335175a03bb590c0f6e344fb
brandonberger <bdonberger@gmail.com>
2017-04-03 20:34:07 -0400

    fuck

--- index.php
+++
@@ -1,7 +0,0 @@
-Project 1 Overview
-<br>
-<br>
-Layout using Bootstrap 3 Admin Template <a target="_blank" href="https://github.com/puikinsh/gentelella">gentelella</a>
-
-<br><br><br>
-<a href="src/">Link to Project1</a>
